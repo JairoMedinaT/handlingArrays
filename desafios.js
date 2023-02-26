58fc8356338ff168ed9037afcd3e0ffb3cb795c0
Jairo Ismael Medina Tarazona. JIMT <jairoismael@hotmail.com>
2023-02-25 19:23:15 -0500

    Desafio 1

---
+++ desafios.js
@@ -0,0 +1,28 @@
+// alert("HOLA");
+
+// DESAFIOS JS
+// Multiplica todos los elementos por dos
+// **********************************************************
+let array = [2, 4, 5, 6, 8];
+
+function multiplyElements(array) {
+    let newArray = [];
+    array.forEach(element => {
+        newArray.push(element * 2);
+    });
+    return console.log("1. Multiplica todos los elementos por dos :" +"\n"+ "El Input es :" + "[" + array + "]" +"\n"+"El Output es : " + "["+newArray+"]"),
+ document.write("1. Multiplica todos los elementos por dos :" +"</br>"+ "El Input es :" + "[" + array + "]" +"</br>"+"El Output es : " + "["+newArray+"]");
+}
+multiplyElements(array);
+// **********************************************************
+
+// asi quedo en la plataforma y paso el desafio
+// *************************
+// export function multiplyElements(array) {
+//     let newArray = [];
+//     array.forEach(element => {
+//        newArray.push(element * 2);
+//     });
+//     return newArray;
+//  }
+// **********************************************************
